6fb0ff4ca17b590d7e937f37794e2bad793e3a58
Давид <davidkozahmetov@MacBook-Air-David.local>
2024-10-18 14:34:17 +0500

    fix header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,8 +15,8 @@ const Header = () => {
   return (
     <header className="w-full min-w-full">
       <div className="top-0 bg-slate-100 flex w-[100vw] h-[180vw] sm:h-[100vw] md:h-[80vw] lg:h-56 items-end lg:items-center lg:justify-end justify-center">
-        <div className="w-[50vw]">
-          <div className="flex justify-center md:justify-start sm:pb-2 pb-36 mb-3">
+        <div className="lg:grid hidden w-[50vw]">
+          <div className="flex justify-center md:justify-start  mb-3">
             <svg
               aria-hidden="true"
               className="h-5 w-5 fill-orange-500"
@@ -53,13 +53,63 @@ const Header = () => {
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
           </div>
-          <p className="text-slate-900 font-bold mb-2 text-center md:text-start absolute m-auto left-7 right-7 top-2/3 sm:top-auto sm:right-auto sm:left-auto sm:static">
-            {t("top.0.about")}
-          </p>
-          <p className="text-slate-400 mb-6 text-center md:text-start absolute m-auto left-7 right-7 top-3/4 sm:top-auto sm:right-auto sm:left-auto sm:static">
-            <span className="text-blue-500">{t("top.0.topName")}</span>{" "}
-            {t("top.0.topDescription")}
-          </p>
+          <div>
+            <p className="text-slate-900 font-bold mb-2 text-center md:text-start absolute m-auto left-7 right-7 top-2/3 sm:top-auto sm:right-auto sm:left-auto sm:static">
+              {t("top.0.about")}
+            </p>
+            <p className="text-slate-400 mb-6 text-center md:text-start absolute m-auto left-7 right-7 top-3/4 sm:top-auto sm:right-auto sm:left-auto sm:static">
+              <span className="text-blue-500">{t("top.0.topName")}</span>{" "}
+              {t("top.0.topDescription")}
+            </p>
+          </div>
+        </div>
+        <div className="lg:hidden grid w-[80vw]">
+          <div className="flex justify-center   mb-3">
+            <svg
+              aria-hidden="true"
+              className="h-5 w-5 fill-orange-500"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              className="h-5 w-5 fill-orange-500"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              className="h-5 w-5 fill-orange-500"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              className="h-5 w-5 fill-orange-500"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              className="h-5 w-5 fill-orange-500"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+          </div>
+          <div>
+            <p className="text-slate-900 font-bold mb-2 text-center">
+              {t("top.0.about")}
+            </p>
+            <p className="text-slate-400 mb-6 text-center">
+              <span className="text-blue-500">{t("top.0.topName")}</span>
+              {t("top.0.topDescription")}
+            </p>
+          </div>
         </div>
       </div>
       <div className="container mx-auto p-2 md:p-8 w-full">
